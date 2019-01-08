ddcfcfee540188a3329924a25feb915c55c109b4
entropicChaos <35173209+entropicChaos@users.noreply.github.com>
2019-01-08 14:33:30 +0100

    Add files via upload

---
+++ getUnicode.py
@@ -0,0 +1,16 @@
+#this program uses python built in functions ord(c)||chr(i) in order to get a unicode #character from an integer or a unicode integer from character
+
+
+question = input('what are you looking for :')
+
+def _to_char_(number):
+	
+
+if (question.lower() == 'i'):
+	intToTranslate = int(input('Enter integer '))
+	intTranslated= chr(intToTranslate)
+	print('%d is equal to %s' %(intToTranslate,intTranslated))
+elif(question.lower()=='c'):
+	charToTranslate = str(input('Enter character '))
+	charTranslated= ord(charToTranslate)
+	print('%s is equal to %d '%(charToTranslate,charTranslated))
